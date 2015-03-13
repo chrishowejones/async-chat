0e52894b8abd84bb5d1c850ab5df9cc051291889
Chris Howe-Jones <chris.howejones@gmail.com>
2015-03-13 20:10:21 +0000

    Simple test echo client and server.

--- src/async_chat/core.clj
+++ src/async_chat/core.clj
@@ -1,5 +1,36 @@
 (ns async-chat.core
-  (:gen-class))
+       (:require [clojure.core.async :refer [<!! >! go ]]
+                [net.async.tcp :refer :all])
+       (:gen-class))
+
+
+(defn echo-server []
+  (let [acceptor (accept (event-loop) {:port 8999})]
+    (loop []
+      (when-let [server (<!! (:accept-chan acceptor))]
+        (go
+          (loop []
+            (when-let [msg (<! (:read-chan server))]
+              (when-not (keyword? msg)
+                (println "msg from client = " (String. msg))
+                (>! (:write-chan server) (.getBytes (str "ECHO/" (String. msg)))))
+              (recur))))
+        (recur)))))
+
+
+(defn echo-client []
+  (let [client (connect (event-loop) {:host "127.0.0.1" :port 8999})]
+    (loop []
+      (go (>! (:write-chan client) (.getBytes (str (rand-int 100000)))))
+      (loop []
+        (let [read (<!! (:read-chan client))]
+          (if (not (keyword? read))
+            (println "Echo from server: " (String. read)))
+          (when (and (keyword? read)
+                     (not= :connected read))
+            (recur))))
+      (Thread/sleep (rand-int 3000))
+      (recur))))
 
 (defn -main
   "I don't do a whole lot ... yet."
